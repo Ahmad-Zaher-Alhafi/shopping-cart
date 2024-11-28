3b7dcc26890e753ca9a77544d9384f3a9a65dee1
Ahmad-Zaher-Alhafi <z1_ahmad@hotmail.com>
2024-11-28 19:16:24 +0300

    NEW: Product component

---
+++ src/Product.jsx
@@ -0,0 +1,19 @@
+import propTypes from "prop-types";
+
+function Product({ imgUrl, name, price }) {
+  return (
+    <div className="product">
+      <img className="=img" src={imgUrl} alt="Product picture" />
+      <div className="name">{name}</div>
+      <div className="price">{price}</div>
+    </div>
+  );
+}
+
+Product.propTypes = {
+  imgUrl: propTypes.string,
+  name: propTypes.string,
+  price: propTypes.string,
+};
+
+export default Product;
--- src/ShoppingPage.jsx
+++ src/ShoppingPage.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import NavgationBar from "./NavigationBar";
+import Product from "./Product";
 
 function ShoppingPage() {
+  const [products, setProducts] = useState([]);
+
   return (
     <div className="shoppingPage">
       <NavgationBar></NavgationBar>
       <h1>Hey shopping page!</h1>
+
+      {products.map((product) => {
+        return (
+          <Product
+            imgUrl={product.key.imgUrl}
+            name={product.name}
+            price={product.price}
+            key={product.key}
+          ></Product>
+        );
+      })}
     </div>
   );
 }
